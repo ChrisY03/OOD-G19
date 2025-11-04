f2c03637b377e86e7f6f4cc0507abfd5e77f6880
lochlancurran1 <lochlancurran33@gmail.com>
2025-11-04 18:31:09 +0000

    Update Room.java

--- Classes/Room.java
+++ Classes/Room.java
@@ -1,3 +1,35 @@
 public class Room {
+    private String roomId;
+    private int capacity;
+    private boolean lab;
+
+    public Room(String roomId, int capacity, boolean lab) {
+        this.roomId = roomId;
+        this.capacity = capacity;
+        this.lab = lab;
+    }
+    public String getRoomId() {
+        return roomId;
+    }
+    public void setRoomId(String roomId) {
+        this.roomId = roomId;
+    }
+    
+    public int getCapacity() {
+        return capacity;
+    }
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public boolean getLab() {
+        return lab;
+    }
+    public void setLab() {
+        this.lab = lab;
+    }
+    public String toString() {
+        return roomId + capacity + lab;
+    }
 
 }
