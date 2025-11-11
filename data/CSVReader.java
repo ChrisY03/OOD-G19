688f2cf104ed1d8caf12b596122fe1d50bc2cf83
lochlancurran1 <lochlancurran33@gmail.com>
2025-11-11 22:39:14 +0000

    Add CSV read/write utilities and student loader
    Introduces CSVReader and CSVWriter classes for reading and writing CSV files. Adds DataManager.loadStudents to parse student data from CSV into Student objects, improving data handling and modularity.

--- data/CSVReader.java
+++ data/CSVReader.java
@@ -1,3 +1,24 @@
-public class CSVReader {
+package data;
 
+import java.io.*;
+import java.util.*;
+
+public class CSVReader {
+    public static List<String[]> readCSV(String filePath) {
+        List<String[]> data = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] row = line.split(",");
+                data.add(row);
+            }
+    } catch (IOException e) {
+        System.err.println("Error reading CSV file: " + filePath);
+        e.printStackTrace();
+    }
+    return data;
+    }
 }
+
+
--- data/CSVWriter.java
+++ data/CSVWriter.java
@@ -1,5 +1,19 @@
 package data;
 
-public class CSVWriter {
+import java.io.*;
+import java.util.*;
 
-}
+public class CSVWriter {
+    public static void writeCSV(String filePath, List<String[]> data) {
+        try (PrintWriter pw = new PrintWriter(new FileWriter(filePath))) {
+            for (String[] row : data) {
+                pw.println(String.join(",", row));
+            }
+        } catch (IOException e) {
+            System.err.println("Error writing to CSV file: " + filePath);
+            e.printStackTrace();
+        }
+    }
+    }
+
+
