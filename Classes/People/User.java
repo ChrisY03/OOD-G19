5d52b89c641b2de88ca379937f6ec494c96a3f31
lochlancurran1 <lochlancurran33@gmail.com>
2025-11-15 12:53:06 +0000

    Add JavaDoc comments to User class
    Added detailed JavaDoc comments to the User class, its constructor, and methods to improve code documentation and clarify the purpose and usage of each attribute and method.

--- Classes/People/User.java
+++ Classes/People/User.java
@@ -1,10 +1,25 @@
+/**
+ * Demonstrates a user (lecturer, student, admin)
+ * works as an abstract superclass for each user type
+ * identifies the key attributes of a user in UL and what
+ * is shared among a lecturer, admin and student
+ */
+
 public abstract class User {
     protected String id;
     protected String name;
     protected String email;
     protected String password;
-    protected String userType; // Student, lecturer, staff
+    protected String userType;
 
+    /**
+     * A constructor (User) with unique attributes of a user in UL
+     * @param id   users specific UL id 
+     * @param name  users name
+     * @param email  users UL email
+     * @param password users password in which they selected
+     * @param userType (Lecturer, student, admin)
+     */
     public User(String id, String name, String email, String password,
      String userType) {
         this.id = id;
@@ -13,20 +28,49 @@ public abstract class User {
         this.password = password;
         this.userType = userType;
      }
+
+     /**
+      * 
+      * @return users specific UL id
+      */
      public String getid() {
         return id;
      }
+
+     /**
+      * 
+      * @return users UL email
+      */
      public String getEmail() {
         return email;
      }
+
+     /**
+      * 
+      * @return users name
+      */
      public String getName() {
         return name;
      }
+
+     /**
+      * 
+      * @return users type (lecturer, student, admin)
+      */
      public String getUserType() {
         return userType;
      }
      // password doesn't get a getter because of security and encapsulation
      // no other class or user should be able to read it directly once its stored
+    
+    /**
+     * represents a login system where the user is prompted to
+     * enter id and password and compares the input to a stored
+     * users info (id, password)
+     * @param identered users id entered
+     * @param passwordEntered users password entered
+     * @return true if login details match, false if no match
+     */
      public boolean idLogin(String identered, String passwordEntered) {
         return this.id.equals(identered) && password.equals(passwordEntered); //Checks if inputted password matches user password;
      }
