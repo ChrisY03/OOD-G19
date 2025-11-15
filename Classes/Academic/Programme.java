6048b648d4addd5a2f15808ee3c211588686f0bc
Pierce74 <pierceriordan12@gmail.com>
2025-11-15 15:26:43 +0000

    Update Programme.java

--- Classes/Academic/Programme.java
+++ Classes/Academic/Programme.java
@@ -1,6 +1,71 @@
 import java.util.ArrayList;
+import java.util.List;
+
 public class Programme {
+
+    /** The programme's unique code. */
     private int code;
+
+    /** The name of the programme. */
     private String name;
-    private List<ProgrammeSemester> = new ArrayList<>();
+
+    /**
+     * The list of semesters in the programme.
+     * Each semester has several modules.
+     */
+    private List<ProgrammeSemester> semesters = new ArrayList<>();
+
+    /**
+     * Creates an empty Programme object.
+     * Allows the programme to be created before its data is available.
+     */
+    public Programme() {
+    }
+
+        /**
+         * Creates a programme with a code and name.
+         * 
+         * @param code the programme code
+         * @param name the programme name
+         */
+    public Programme(int code, String name){
+            this.code = code;
+            this.name = name;
+    }
+
+    /**
+     * Returns the programme's code.
+     * 
+     * @return programme code
+     */
+    public int getCode() {
+        return code;
+    }
+
+    /**
+     * Returns the programme's name.
+     * 
+     * @return programme name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Returns the list of semesters in the programme.
+     * 
+     * @return list of ProgrammeSemester objects
+     */
+    public List<ProgrammeSemester> getSemesters() {
+        return semesters;
+    }
+
+    /**
+     * Adds a semester to the programme.
+     * 
+     * @param semester the semester to add
+     */
+    public void addSemester(ProgrammeSemester semester) {
+        semesters.add(semester);
+    }
 }
