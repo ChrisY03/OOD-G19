b24047609e61d5c694b3613827a502899b28fefa
Pierce74 <pierceriordan12@gmail.com>
2025-11-15 19:44:30 +0000

    Update CSVWriter.java

--- data/CSVWriter.java
+++ data/CSVWriter.java
@@ -3,7 +3,17 @@ package data;
 import java.io.*;
 import java.util.*;
 
+/** A utility class for writing data to CSV (comma-separated value) files.
+ * Each row is written as a comma-separated String array.
+ */
 public class CSVWriter {
+
+    /**
+     * Writes data to a CSV file at the given file path.
+     *
+     * @param filePath the path to the output CSV file
+     * @param data a list of String arrays, where each array represents one row
+     */
     public static void writeCSV(String filePath, List<String[]> data) {
         try (PrintWriter pw = new PrintWriter(new FileWriter(filePath))) {
             for (String[] row : data) {
@@ -14,6 +24,6 @@ public class CSVWriter {
             e.printStackTrace();
         }
     }
-    }
+}
 
 
