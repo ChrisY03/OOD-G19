7ef28a01ab8345bfe5e4ce8b49bd9a7d135e3efa
Pierce74 <pierceriordan12@gmail.com>
2025-11-16 00:05:53 +0000

    .

--- Classes/People/User.java
+++ Classes/People/User.java
@@ -1,3 +1,5 @@
+package People;
+
 /**
  * Demonstrates a user (lecturer, student, admin)
  * works as an abstract superclass for each user type
@@ -14,55 +16,67 @@ public abstract class User {
 
     /**
      * A constructor (User) with unique attributes of a user in UL
-     * @param id   users specific UL id 
+     * @param id   users specific UL id
      * @param name  users name
      * @param email  users UL email
      * @param password users password in which they selected
      * @param userType (Lecturer, student, admin)
      */
     public User(String id, String name, String email, String password,
-     String userType) {
+                String userType) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.password = password;
         this.userType = userType;
-     }
+    }
 
-     /**
-      * 
-      * @return users specific UL id
-      */
-     public String getid() {
+    /**
+     * Creates a simplified User object.
+     * This constructor is used when only the user's name and role are needed,
+     * without ID, email or password details.
+     * @param name the user's name
+     * @param userType the role of the user
+     */
+    public User(String name, String userType) {
+        this.name = name;
+        this.userType = userType;
+    }
+
+    /**
+     *
+     * @return users specific UL id
+     */
+    public String getid() {
         return id;
-     }
+    }
 
-     /**
-      * 
-      * @return users UL email
-      */
-     public String getEmail() {
+    /**
+     *
+     * @return users UL email
+     */
+    public String getEmail() {
         return email;
-     }
+    }
 
-     /**
-      * 
-      * @return users name
-      */
-     public String getName() {
+    /**
+     *
+     * @return users name
+     */
+    public String getName() {
         return name;
-     }
+    }
 
-     /**
-      * 
-      * @return users type (lecturer, student, admin)
-      */
-     public String getUserType() {
+    /**
+     *
+     * @return users type (lecturer, student, admin)
+     */
+    public String getUserType() {
         return userType;
-     }
-     // password doesn't get a getter because of security and encapsulation
-     // no other class or user should be able to read it directly once its stored
-    
+    }
+    // password doesn't get a getter because of security and encapsulation
+    // no other class or user should be able to read it directly once its stored
+
     /**
      * represents a login system where the user is prompted to
      * enter id and password and compares the input to a stored
@@ -71,13 +85,18 @@ public abstract class User {
      * @param passwordEntered users password entered
      * @return true if login details match, false if no match
      */
-     public boolean idLogin(String identered, String passwordEntered) {
+    public boolean idLogin(String identered, String passwordEntered) {
         return this.id.equals(identered) && password.equals(passwordEntered); //Checks if inputted password matches user password;
-     }
+    }
 
-     
+    /**
+     * Each user type must define how they view a timetable.
+     */
+    public abstract void viewTimetable();
 
-     
+
+
+
 
 
 
--- data/DataManager.java
+++ data/DataManager.java
@@ -3,10 +3,24 @@ package data;
 import People.Student;
 import java.util.*;
 
+/**
+ * The DataManager class is responsible for loading data from CSV files 
+ * and converting it into objects used by the system.
+ */
 public class DataManager {
+
+    /**
+     * Loads student data from a CSV file and converts each row into a 
+     * Student object.
+     * 
+     * Expected CSV fromat: ID, Name, Email, Password, ProgrammeID, Year, Group
+     * 
+     * @param filePath the path to the CSV file
+     * @return a list of Student objects
+     */
     public static List<Student> loadStudents(String filePath) {
         List<Student> students = new ArrayList<>();
-        List<String[] data = CSVReader.readCSV(filePath);
+        List<String[]> data = CSVReader.readCSV(filePath);
 
         for (String[] row : data) {
             String id = row[0];
