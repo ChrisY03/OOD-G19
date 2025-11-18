f2f0f117e2c20709fc0164930d133773b9d59685
ChrisY03 <christophermyoung05@gmail.com>
2025-11-18 14:18:14 +0000

    Refactor ScheduledSession to use domain objects
    Replaced String fields in ScheduledSession with Module, Lecturer, Room, and Timeslot objects for better type safety and domain modeling. Updated constructor accordingly. Added placeholder for TimetableService.java.

---
+++ src/controllers/TimetableService.java
@@ -0,0 +1,63 @@
+package src.controllers;
+
+public class TimetableService {
+    private List<ScheduledSessions> allSessions;
+    private List<Student> students;
+    private List<Room> rooms;
+    private List<Programme> programmes;
+
+    //Querying timetables
+    public void getAllSessions extends DataManager(){
+
+    }
+
+    public void getSessionId(sessionId){
+
+    }
+    
+    public void getStudentTimetable(studentId){
+
+    }
+
+    public void getLecturerTimetable(lecturerId){
+
+    }
+
+    public void getRoomTimetable(roomCode){
+
+    }
+
+    public void getProgrammeTimetable(programmeCode, semester){
+
+    }
+
+    public void getModuleSessions(moduleCode){
+
+    }
+
+    //Methods to write/change timetables
+    public void addSession(ScheduledSession newSession){
+
+    }
+
+    public void updateSession(ScheduledSession updatedSession){
+
+    }
+
+    public void deleteSession(int sessionId){
+
+    }
+    
+    public void saveTimetable(){
+
+    }
+
+    //Useful for Scheduler
+    public void getSessionsForTimeandRoom(day, start, end, roomCode){ }
+
+    public void getSessionsLecturer(day, start, end, lecturerId){ }
+
+    public void getSessionsForAudience(day, start, end, audienceId){ }
+
+    
+}
